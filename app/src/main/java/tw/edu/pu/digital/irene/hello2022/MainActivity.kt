68631de59e285bc2ff1d9c4eacec6e499f710590
Irene Yii <yiizhengcy1718@gmail.com>
2022-03-09 10:57:06 +0800

    第一次commit

---
+++ app/src/main/java/tw/edu/pu/digital/irene/hello2022/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.edu.pu.digital.irene.hello2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
