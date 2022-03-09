4637c057a27b92911b66abdc362e3757f3da2fc5
Irene Yii <yiizhengcy1718@gmail.com>
2022-03-09 11:23:14 +0800

    master

--- app/src/main/java/tw/edu/pu/digital/irene/hello2022/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/digital/irene/hello2022/MainActivity.kt
@@ -9,6 +9,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var txv:TextView = findViewById<TextView>(R.id.txv)
         txv.text = "Hello tcyang"
-
+        TestMaster()
     }
+    fun TestMaster(){var txv:TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "Master分支"}}
+
+}
 }
