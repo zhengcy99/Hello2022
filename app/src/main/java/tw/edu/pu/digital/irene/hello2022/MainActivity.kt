a64a9adb3c1857f232209c748b005b24c4d30285
Irene Yii <yiizhengcy1718@gmail.com>
2022-03-09 11:20:51 +0800

    第二次commit

--- app/src/main/java/tw/edu/pu/digital/irene/hello2022/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/digital/irene/hello2022/MainActivity.kt
@@ -9,6 +9,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var txv:TextView = findViewById<TextView>(R.id.txv)
         txv.text = "Hello tcyang"
-
+        TestDEV()
     }
+    fun TestDEV(){var txv:TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "Dev分支"
+    }
+}
+
+
+}
 }
